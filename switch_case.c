0bacbfaad031697e8ca4b86df72257e32e2fbc37
serhatdgd <dogduserhat2002@gmail.com>
2023-07-14 14:07:46 +0300

    first commit

--- switch_case.c
+++ switch_case.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int day=4;
+    switch(day)
+    {
+        case 1: printf("Monday\n");
+            break;
+        case 2: printf("Tuesday\n");
+            break;
+        case 3: printf("Wednesday\n");
+            break;
+        case 4: printf("Thursday\n");
+            break;
+        case 5: printf("Friday\n");
+            break;
+        case 6: printf("Saturday\n");
+            break;
+        case 7: printf("Sunday\n");
+            break;
+        default:printf("There is no such day \n");
+    }
+}
