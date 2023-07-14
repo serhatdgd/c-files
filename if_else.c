cc27d608f96b2253721af779b67c3c61ea76fe92
serhatdgd <dogduserhat2002@gmail.com>
2023-07-14 10:49:21 +0300

    first commit

--- if_else.c
+++ if_else.c
@@ -3,5 +3,17 @@
 
 int main()
 {
-    
+    int score;
+    printf("Score: ");
+    scanf("%d",&score);
+    if(score>=60){
+        printf("Congratulations you passed the exam \n");
+        printf("Your score: %d\n\n",score);
+    }
+    else{
+        printf("Unfortunately you did not pass the exam\n");
+        printf("Your score: %d\n\n",score);
+    }
+    printf("The program continues to run \n\n");
+    return 0;
 }
